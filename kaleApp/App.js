4c45ddaa17a45a61b56ba71cc7cec4e7d9375f54
Esinam18 <he.kale@st.ug.edu.gh>
2024-05-27 13:33:05 +0000

    bolding text

--- kaleApp/App.js
+++ kaleApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.fontEdit}>
+        My name is <Text style={styles.fontBold}> Harriet Esinam Kale </Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fontEdit:{
+    fontSize: 24,
+  },
+  fontBold:{
+    fontWeight: 'bold',
   },
 });
